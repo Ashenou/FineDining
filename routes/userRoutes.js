2ac1365b9d996412327d64a60d2f243a9ad511fe
Abanoub Shenouda <Abanoub Shenouda>
2021-06-29 00:02:32 -0400

    Add menu display and user submit order structure in userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,35 +1,47 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-var cookie = require('cookie')
-
-
-
 
 // /user/login
 module.exports = (db) => {
-   // GET / -- View login page
+
+
+  // GET / -- View login page
   router.get("/", (req, res) => {
     res.render("login");
-  })
+  });
+
   // POST / -- Checks if user exists in database
-  router.post("/", (req,res) => {
+  router.post("/", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    db.query(`SELECT * FROM users WHERE email = $1 AND password = $2;`, [email, password])
-    .then((result) => {
-      //console.log(result.rows[0])
-      res.cookie('user', result.rows[0])
-      return res.redirect('/');
-    })
-    .catch((err) => {
-    console.log(err.message);
-    })
-});
-return router;
-}
+    db.query(`SELECT * FROM users WHERE email = $1 AND password = $2;`, [
+      email,
+      password,
+    ])
+      .then((result) => {
+        //console.log(result.rows[0])
+        res.cookie("user", result.rows[0]);
+        return res.redirect("/");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
 
 
+  // GET :userid/orders Shows restaurant's menu to user
+  router.get("/orders", (req, res) => {
+    db.query();
+  });
 
+  // POST :userid/orders Submits users' order
+  router.post("/orders", (req, res) => {
+    db.query();
+  });
+
+
+  return router;
+};
 
 // module.exports = (db) => {
 // //   router.get("/", (req, res) =>
--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 // load .env data into process.env
-require('dotenv').config();
+require("dotenv").config();
 
 // Web server config
-const PORT       = process.env.PORT || 8080;
-const ENV        = process.env.ENV || "development";
-const express    = require("express");
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || "development";
+const express = require("express");
 const bodyParser = require("body-parser");
 // const sass       = require("node-sass-middleware");
-const app        = express();
-const morgan     = require('morgan');
+const app = express();
+const morgan = require("morgan");
 
 // PG database client/connection setup
-const { Pool } = require('pg');
-const dbParams = require('./lib/db.js');
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
 const db = new Pool(dbParams);
 db.connect();
-const fs = require("fs")
+const fs = require("fs");
 const sass = require("sass");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -33,15 +33,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // }));
 
 app.get("/styles/:css_file", (req, res, next) => {
-  const cssFilename = req.params.css_file.replace(/\.[^/.]+$/, "")
+  const cssFilename = req.params.css_file.replace(/\.[^/.]+$/, "");
   const rendered = sass.renderSync({
     file: `${__dirname}/styles/${cssFilename}.scss`,
     outFile: `${__dirname}/public/styles/${cssFilename}.css`,
     debug: true,
-    outputStyle: 'expanded'
-  })
-  fs.writeFileSync(`${__dirname}/public/styles/${cssFilename}.css`, rendered.css.toString())
-  next()
+    outputStyle: "expanded",
+  });
+  fs.writeFileSync(
+    `${__dirname}/public/styles/${cssFilename}.css`,
+    rendered.css.toString()
+  );
+  next();
 });
 
 app.use(express.static("public"));
@@ -49,22 +52,20 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 // const usersRoutes = require("./routes/users");
-const apiRoutes = require('./routes/apiRoutes')
-app.use('/', apiRoutes);
+const apiRoutes = require("./routes/apiRoutes");
+app.use("/", apiRoutes);
 
-const restaurantRoutes = require('./routes/restaurantRoutes')
+const restaurantRoutes = require("./routes/restaurantRoutes");
 // app.use('/', homePageRoutes);
 
-const userRoutes = require('./routes/userRoutes')
-app.use('/login', userRoutes(db));
-
+const userRoutes = require("./routes/userRoutes");
+app.use("/login", userRoutes(db));
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // app.use("/api/users", usersRoutes(db));
 
 // Note: mount other resources here, using the same pattern above
-
 
 // Home page
 // Warning: avoid creating more routes in this file!
