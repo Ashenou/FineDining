eb882f1d338bedfdb54312f8e157ec33856dfc59
Abanoub Shenouda <Abanoub Shenouda>
2021-06-29 19:01:40 -0400

    refactor restaurant menu display in ItemsRoutes

--- routes/itemsRoutes.js
+++ routes/itemsRoutes.js
@@ -4,23 +4,22 @@ const router = express.Router();
 module.exports = (db) => {
   // GET /login -- View login page
   router.get("/", (req, res) => {
-
     let templateVars = {};
 
+    //// implement login login with req.id and req.restaurant_check
+
     db.query(
-      `SELECT *
+      `SELECT items.* , item_type.name as item_type_name
     FROM items
     JOIN item_type ON items.type_id = item_type.id ;`
     )
       .then((result) => {
-
         //console.log(result.rows);
         templateVars["items"] = result.rows;
-        console.log("line 18",templateVars["items"]);
-        return res.render("items",templateVars);
+        console.log("line 18", templateVars["items"]);
+        return res.render("items", templateVars);
       })
       .catch((err) => console.log(err));
-
   });
 
   // POST /login -- Checks if user exists in database
