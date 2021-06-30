339f5449b6c1577a25364f34651c31f1862d08e2
Abanoub Shenouda <Abanoub Shenouda>
2021-06-30 15:46:20 -0400

    Add user order submit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,15 +2,14 @@ const twilio = require("twilio");
 const express = require("express");
 const router = express.Router();
 
-// Twilio api require
-
 module.exports = (db, accountSid, authToken) => {
   // GET /login -- View login page
+
   router.get("/login", (req, res) => {
-    return res.render("login");
+    let templateVars = { user: req.cookies.user };
+    res.render("login", templateVars);
   });
 
-  // POST /login -- Checks if user exists in database
   router.post("/login", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
@@ -19,7 +18,6 @@ module.exports = (db, accountSid, authToken) => {
       password,
     ])
       .then((result) => {
-        //console.log(result.rows[0])
         res.cookie("user", result.rows[0]);
         return res.redirect("/");
       })
@@ -28,15 +26,10 @@ module.exports = (db, accountSid, authToken) => {
       });
   });
 
-  //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-  //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-  //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-  // GET :userid/orders Shows restaurant's menu to user
-  ////// Correct route for GET AND POST to "/:id/orders"
+  /// GET :userid/items Shows restaurant's menu to user
   router.get("/items", (req, res) => {
     //// implement login login with req.id and req.restaurant_check
-    let userId = req.cookies;
+    let userId = req.cookies["user"].id;
     console.log("Line 62 ", userId);
 
     let templateVars = {};
@@ -46,17 +39,13 @@ module.exports = (db, accountSid, authToken) => {
     JOIN item_type ON items.type_id = item_type.id ;`
     )
       .then((result) => {
-        //console.log(result.rows);
         templateVars["items"] = result.rows;
-        console.log("line 18", templateVars["items"]);
         return res.render("items", templateVars);
       })
       .catch((err) => console.log(err));
   });
 
-  //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-  // POST :userid/orders Submits users' order
+  //// POST :userid/orders Submits users' order
   // async here is to be able to use await for the variable
   router.post("/items", async (req, res) => {
     // insert a new order in database and get back the id;
@@ -65,8 +54,6 @@ module.exports = (db, accountSid, authToken) => {
     let restaurantId = 1;
     let userId = 1;
     let orderFilled = false;
-    //userId = req.cookies;
-    console.log("Line 62 ", userId);
 
     let createdOrderId = await db
       .query(
@@ -78,14 +65,10 @@ module.exports = (db, accountSid, authToken) => {
       })
       .catch((err) => console.log(err.message));
 
-    // [ ] Finalize adding items to order_items table
     console.log("createdOrderId :>>zz ", createdOrderId);
 
     // loop through all items in the form -- item is id of item in database and it's value is the quantity
     for (const item in req.body) {
-      //console.log("Line 81 This is item id in database",item);
-      //console.log("Line 82 This is the quantity of items in database",req.body[item]);
-
       // Loop through each item in the form and get back the quantity for each
       if (req.body[item] !== "") {
         orderFilled = true;
@@ -94,13 +77,12 @@ module.exports = (db, accountSid, authToken) => {
             `INSERT INTO order_items(order_id,item_id,customer_id,restaurant_id) VALUES($1,$2,$3,$4)`,
             [createdOrderId, item, userId, restaurantId]
           )
-            .then(() => {
-              console.log("👍  order was submitted successfully");
-            })
+            .then(() => {})
             .catch((err) => console.log("line 74", err.message));
         }
       }
     }
+    // Checks if order was not empty
     if (orderFilled) {
       // Twilio Implementation
       const twilioClient = new twilio(accountSid, authToken);
@@ -108,7 +90,7 @@ module.exports = (db, accountSid, authToken) => {
       twilioClient.messages
         .create({
           body: `Your order was created please wait 45 minutes until it's ready 👻`,
-          to: "+13439976090", // Text this number
+          to: `${req.cookies["user"].phone_number}`, // Text this number
           from: "+16572247880", // From a valid Twilio number
         })
         .then((message) => message.sid);
@@ -117,39 +99,3 @@ module.exports = (db, accountSid, authToken) => {
   });
   return router;
 };
-
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Find your Account SID and Auth Token at twilio.com/console
-// and set the environment variables. See http://twil.io/secure
-// const accountSid = process.env.TWILIO_ACCOUNT_SID;
-// const authToken = process.env.TWILIO_AUTH_TOKEN;
-// const client = require('twilio')(accountSid, authToken);
-
-// client.messages
-//   .create({
-//      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-//      from: '+16572247880',
-//      to: '+15558675310'
-//    })
-//   .then(message => console.log(message.sid));
-
-// module.exports = (db) => {
-// //   router.get("/", (req, res) =>
-// //   {
-// //       res.render("index");
-// //     // db.query(`SELECT * FROM users;`)
-// //     //   .then(data => {
-// //     //     const users = data.rows;
-// //     //     res.json({ users });
-// //     //   })
-// //     //   .catch(err => {
-// //     //     res
-// //     //       .status(500)
-// //     //       .json({ error: err.message });
-// //     //   });
-// //   });
-// router.get("/", (req, res) => {
-//   res.render("index");
-// });
-//   return router;
-// };
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 8080;
 const ENV = process.env.ENV || "development";
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 // const sass       = require("node-sass-middleware");
 const app = express();
 const morgan = require("morgan");
@@ -28,6 +29,7 @@ app.use(morgan("dev"));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 // app.use("/styles", sass({
 //   src: __dirname + "/styles",
 //   dest: __dirname + "/public/styles",
