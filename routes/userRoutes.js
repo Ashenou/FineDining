62489e5fd09c6bcebb41fa86c1e7ef32511e7c7f
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-06-30 21:35:44 +0000

    update route / to /login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
-var cookie = require('cookie')
+//var cookie = require('cookie')
 
 // /user/login
 module.exports = (db) => {
   // GET /login -- View login page
-  router.get("/", (req, res) => {
-    templateVars = { user: req.cookies.user }
+  router.get("/login", (req, res) => {
+    let templateVars = { user: req.cookies.user }
     res.render("login", templateVars);
   });
 
   // POST /login -- Checks if user exists in database
-  router.post("/", (req, res) => {
+  router.post("/login", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
     db.query(`SELECT * FROM users WHERE email = $1 AND password = $2;`, [
