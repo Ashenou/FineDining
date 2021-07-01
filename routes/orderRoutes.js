19688d2361484647bca9c59e8aa7dcd105142f47
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-07-01 02:42:19 +0000

    add twilio implementation

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,10 +1,10 @@
+const twilio = require("twilio");
 const express = require('express');
 const router = express.Router();
-var cookie = require('cookie');
 
-const { formatArrayObject, orderTimer } = require('../public/helper/helper')
+const formatArrayObject = require('../public/helper/Format_ArrayObject')
 
-module.exports = (db) => {
+module.exports = (db, accountSid, authToken) => {
   // GET :/orders Display's' orders for the restaurant portal
   router.get("/", (req, res) => {
     let user = req.cookies.user;
@@ -48,7 +48,23 @@ module.exports = (db) => {
       SET
       accepted_at = $1
       WHERE
-      id = $2;`, [new Date().toISOString().slice(0, 19).replace("T", " "), req.body.orderId]).then((result) => {
+      id = $2 RETURNING *;`, [new Date().toISOString().slice(0, 19).replace("T", " "), req.body.orderId]).then((updateResult) => {
+
+        //Query for getting user details for sending a sms.
+        db.query(`SELECT * FROM users
+        JOIN orders ON user_id=users.id
+        WHERE orders.id=$1;`, [req.body.orderId]).then((resultUserOrder) => {
+          const twilioClient = new twilio(accountSid, authToken);
+
+          twilioClient.messages
+            .create({
+              body: `Your food is being prepared stay tuned!`,
+              to: `${resultUserOrder.rows[0].phone_number}`, // Text this number
+              from: "+13078777489", // From a valid Twilio number
+            })
+            .then((message) => message.sid);
+        });
+
 
         db.query(`SELECT orders.id,created_at,completed_at,accepted_at, user_id, (items.name) as item_name
           FROM orders
@@ -57,10 +73,10 @@ module.exports = (db) => {
           JOIN items on items.id = order_items.item_id
           WHERE completed_at IS NULL
           GROUP BY orders.id,item_name,user_id;`)
-          .then((result) => {
+          .then((resultAllOrders) => {
 
             //formatting the Array of objects
-            const data = formatArrayObject(result.rows);
+            const data = formatArrayObject(resultAllOrders.rows);
             const templateVars = {
               data
             }
