d7c154818fc12ac8ff344cf2086f3c03e307195b
Abanoub Shenouda <Abanoub Shenouda>
2021-07-01 21:02:31 -0400

    fix typos in menu

--- db/seeds/items_types.sql
+++ db/seeds/items_types.sql
@@ -2,5 +2,5 @@ DELETE FROM item_type;
 
 INSERT INTO item_type (name) VALUES ('entree');
 INSERT INTO item_type (name) VALUES ('appetizer');
-INSERT INTO item_type (name) VALUES ('desert');
+INSERT INTO item_type (name) VALUES ('dessert');
 INSERT INTO item_type (name) VALUES ('beverage');
